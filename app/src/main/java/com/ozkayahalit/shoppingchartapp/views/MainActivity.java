1a9d1455a92c1b188cf73b133d2e27eec1be9919
indirjavam <indirjava@gmail.com>
2021-01-06 00:42:43 -0500

    Badge on Menu Item $ Change Color if it is Available or Out of stock

--- app/src/main/java/com/ozkayahalit/shoppingchartapp/views/MainActivity.java
+++ app/src/main/java/com/ozkayahalit/shoppingchartapp/views/MainActivity.java
@@ -1,9 +1,10 @@
 package com.ozkayahalit.shoppingchartapp.views;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     NavController navController;
     ShopViewModel shopViewModel;
 
+    private int cartQuantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,8 +38,14 @@ public class MainActivity extends AppCompatActivity {
         shopViewModel.getCart().observe(this, new Observer<List<CartItem>>() {
             @Override
             public void onChanged(List<CartItem> cartItems) {
-                Log.d(TAG,"onChanged: " + cartItems.size());
+//                Log.d(TAG,"onChanged: " + cartItems.size());
 
+                int quantity = 0;
+                for (CartItem cartItem: cartItems) {
+                    quantity += cartItem.getQuantity();
+                }
+                cartQuantity = quantity;
+                invalidateOptionsMenu();
             }
         });
     }
@@ -50,6 +59,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.cartFragment);
+        View actionView = menuItem.getActionView();
+
+        TextView cartBadgeTextView = actionView.findViewById(R.id.cart_badge_text_view);
+
+        cartBadgeTextView.setText(String.valueOf(cartQuantity));
+        cartBadgeTextView.setVisibility(cartQuantity == 0 ? View.GONE : View.VISIBLE);
+
+        actionView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onOptionsItemSelected(menuItem);
+            }
+        });
+
+
         return true;
     }
 
